e4492a1a664a579c1179a639bbfb33b7307f45a9
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2013-12-12 20:20:47 +0800

    37signals revisited
    Added arrows and JS file

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$('#basecamp').hover(function () {
+		$('#basecamp-hover, .header').toggle();
+	});
+	$('#highrise').hover(function () {
+		$('#highrise-hover, .header').toggle();
+	});
+	$('#campfire').hover(function () {
+		$('#campfire-hover, .header').toggle();
+	});
+});
